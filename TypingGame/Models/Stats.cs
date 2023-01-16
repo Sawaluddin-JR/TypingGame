61a14ae9296d1126bc77d263bba4ea2b5123d518
asus <asus@LAPTOP-ILEMOPHU>
2023-01-16 07:56:55 +0700

    Add project files.

---
+++ TypingGame/Models/Stats.cs
@@ -0,0 +1,33 @@
+﻿namespace TypingGame.Models
+{
+    public class Stats
+    {
+        //inisialisasi variabel secara public
+        public int Correct { get; set; }
+        public int Missed { get; set; }
+        public int Total
+        {
+            get
+            {
+                return Correct + Missed;
+            }
+            set
+            {
+                { Correct = value; }
+            }
+        }
+        public int Accuaracy;
+
+        //methode menghitung total ketikan
+        public void CountTotal()
+        {
+            Total = Correct + Missed;
+        }
+
+        //methode menghitung persentasi
+        public void CountAccuaracy() 
+        {
+            Accuaracy = Correct * 100 / (Correct + Missed);
+        }
+    }
+}
